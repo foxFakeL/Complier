7831f6d8488ecfa7812853876d1cdcf2e9b032e1
foxFakeL <3196718529@qq.com>
2023-06-11 15:16:09 +0800

    init

---
+++ build/CMakeFiles/Complier.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Complier.dir/main.cpp.o"
+  "CMakeFiles/Complier.dir/main.cpp.o.d"
+  "Complier"
+  "Complier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Complier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/foxfake/Code/LR1Praser/src
+# Build directory: /home/foxfake/Code/LR1Praser/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
