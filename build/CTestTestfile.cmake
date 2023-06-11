6600ce21cfa4e0f224c584e09c3f8a791d7f3176
foxFakeL <3196718529@qq.com>
2023-06-11 15:19:51 +0800

    init

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/foxfake/Code/LR1Praser/src
-# Build directory: /home/foxfake/Code/LR1Praser/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
